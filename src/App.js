3da217eba3a02ea335143d68a8fac9d82b448624
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-02 07:31:08 -0500

    added an g-o-thrones class, a try, an await, .setState and an (e) and a clog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      date: null,
+      url: 'http://anapioficeandfire.com/api/characters/583',
+      character: [],
+     };
+  }
 
-    </div>
-  );
+  // 
+  async gameofT(){
+    // try --> basically an upper eshelon if statement
+    // await --> waits for the async func??
+    // this.setState --> update the information from the API
+    // catch --> catches the error if try fails
+    try {
+      const rungameofT = await axios.get('http://anapioficeandfire.com/api/characters/583')
+      this.setState ({ character: rungameofT.data });
+    } catch(e){
+      console.log(`something went wrong`);
+    }
+  }
+  render() {
+    return (
+    <h1> John Snow was born in thi{this}</h1>
+    );
+  }
 }
 
 export default App;
