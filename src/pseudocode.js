b3dd2aaa251481a8e06524b96c50cae28876d79b
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-03 18:40:38 -0500

    converted psuedo file to REFERENCE file

--- src/pseudocode.js
+++ src/pseudocode.js
@@ -1,52 +1,13 @@
-// import React from 'react';
-// import './App.css';
-// import axios from 'axios';
+                        // REFERENCES
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { 
-//       // data: true,
-//       character: [],
-//      };
-//   }
+/*
 
-//   // like the componentDidMount() {}
-//   // use async in order to access fetch or the URL
-//   async gameofT(){
-//     // try --> basically an upper eshelon if statement
-//     // await --> waits for the async func??
-//     // this.setState --> update the information from the API
-//       // json () a asynchronous call 
-//       // ({ update character with the data from the object })
-//     // catch --> catches the error if try fails
-//     try {
-//       const rungameofT = await axios.get('http://anapioficeandfire.com/api/characters/583')
-//       this.setState ({ character: rungameofT.results });
-//     } catch(e){
-//       console.log(`something went wrong`);
-//     }
-//   }
+FETCH from API 
+https://www.youtube.com/watch?v=T3Px88x_PsAlist=PLip2BT2JcqiDq0ipI4DlxVXe9x-2KI03s&index=3&t=0s
 
-// johnSnow(){
-//   // axios gets whats inside the data property
-//   // res and res.data --> to get the results of the in url
-//   //
-//   axios.get('http://anapioficeandfire.com/api/characters/583')
-//         .then((res => console.log(res.data))
-//         ).catch();
-// }
 
-//   render() {
-//     return (
-//       // render the data I found in the url for Jon snow's birth
-//       // how though? --> maybe a this.setState
-//       <div>
-//     <h1> John Snow was born in {this.state.character}</h1> 
-//     {/* watch a video on api's / axios and DOM */}
-//     </div>
-//     );
-//   }
-// }
 
-// export default App;
+
+
+
+*/
